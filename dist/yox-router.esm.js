5975edb1192d5e716099da3c6d69a664e76ee266
musicode <musicode@qq.com>
2019-07-28 19:15:49 +0800

    bump 1.0.0-alpha.45
    配置 Yox 升级

--- dist/yox-router.esm.js
+++ dist/yox-router.esm.js
@@ -1,5 +1,5 @@
 /**
- * yox-router.js v1.0.0-alpha.44
+ * yox-router.js v1.0.0-alpha.45
  * (c) 2017-2019 musicode
  * Released under the MIT License.
  */
@@ -894,7 +894,7 @@ const default404 = {
 /**
  * 版本
  */
-const version = "1.0.0-alpha.44";
+const version = "1.0.0-alpha.45";
 /**
  * 安装插件
  */
