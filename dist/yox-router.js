d73b8b8522ef2bf008e64cebd1b9c0f4c39e4a6a
musicode <musicode@qq.com>
2021-09-22 19:01:24 +0800

    bump 1.0.0-alpha.124

--- dist/yox-router.js
+++ dist/yox-router.js
@@ -1,5 +1,5 @@
 /**
- * yox-router.js v1.0.0-alpha.123
+ * yox-router.js v1.0.0-alpha.124
  * (c) 2017-2021 musicode
  * Released under the MIT License.
  */
@@ -965,7 +965,7 @@
   /**
    * 版本
    */
-  var version = "1.0.0-alpha.123";
+  var version = "1.0.0-alpha.124";
   /**
    * 安装插件
    */
