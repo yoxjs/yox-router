1b34fdb0a8bff1ceb26ea494015f2e24c73940d6
musicode <musicode@qq.com>
2022-04-10 20:54:39 +0800

    bump 1.0.0-alpha.154

--- dist/yox-router.esm.js
+++ dist/yox-router.esm.js
@@ -1,5 +1,5 @@
 /**
- * yox-router.js v1.0.0-alpha.153
+ * yox-router.js v1.0.0-alpha.154
  * (c) 2017-2022 musicode
  * Released under the MIT License.
  */
@@ -940,7 +940,7 @@ Yox.lifeCycle
 /**
  * 版本
  */
-const version = "1.0.0-alpha.153";
+const version = "1.0.0-alpha.154";
 /**
  * 安装插件
  */
